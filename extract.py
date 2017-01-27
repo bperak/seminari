3bef54f55b0e5a754fd7947713a0b2550d019deb
Nikola Ljubešić <nljubesi@gmail.com>
2017-01-27 17:09:51 +0100

    Discretization to second tsv added

--- extract.py
+++ extract.py
@@ -76,7 +76,7 @@ for file in os.listdir(directory):
         output1.append((directory+'.'+file,directory,no_tokens,divide_zero(len(types),no_tokens),no_sents,divide_zero(no_tokens,no_sents),divide_zero(no_lexical,no_words),divide_zero(freq_words,no_words),divide_zero(freq_lemmas,no_words)))
         output2.append((directory+'.'+file,directory,no_words,divide_zero(word_len,no_words),divide_zero(no_infs,no_verbs),divide_zero(no_trebati3s,no_trebati),divide_zero(no_da,no_words*10000),divide_zero(no_nouns,no_nouns+no_verbs)))
 add1=zip(diskretiziraj_granicnima([e[5] for e in output1],[20,30,40,50]),diskretiziraj_granicnima([e[6] for e in output1],[0.5,0.6,0.7]),diskretiziraj_granicnima([e[7] for e in output1],[0.3,0.4,0.5]),diskretiziraj_granicnima([e[8] for e in output1],[0.44,0.59]))
-add2=diskretiziraj_granicnima([e[3] for e in output2],[20,30,40,50])
+add2=diskretiziraj_granicnima([e[3] for e in output2],[4.5,5.5])
 for o,a in zip(output1,add1):
     file1.write('\t'.join([str(e) for e in o])+'\t'+'\t'.join([str(e) for e in a])+'\n')
 for o,a in zip(output2,add2):
